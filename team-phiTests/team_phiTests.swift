d33101aa1e381a62ed56fac07468b5822a89f673
atomsprint <atoms_print@studyatoms.com>
2026-01-05 16:07:44 -0800

    Add files via upload
    initial commit

---
+++ team-phiTests/team_phiTests.swift
@@ -0,0 +1,17 @@
+//
+//  team_phiTests.swift
+//  team-phiTests
+//
+//  Created by user on 2025/12/27.
+//
+
+import Testing
+@testable import team_phi
+
+struct team_phiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
