59cdd2eb946d2762fd96eb57c1ef4757bc37f8ac
kokoan111 <mike.tama.tora@gmail.com>
2026-02-21 06:12:07 +0900

    よろしくお願いします

--- team-phi/viewcontoroller.swift
+++ team-phi/viewcontoroller.swift
@@ -9,9 +9,119 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func dragView(_ sender: Any) {
+    }
+    @IBAction func dragView2(_ sender: Any) {
+    }
+    @IBAction func dragView3(_ sender: Any) {
+    }
+    @IBAction func dragView4(_ sender: Any) {
+    }
+    @IBAction func dragView5(_ sender: Any) {
+    }
+    @IBAction func dragView6(_ sender: Any) {
+    }
+    @IBAction func dragView7(_ sender: Any) {
+    }
+    @IBAction func dragView8(_ sender: Any) {
+    }
+    @IBAction func dragView9(_ sender: Any) {
+    }
+    @IBAction func dragView10(_ sender: Any) {
+    }
+    @IBAction func dragView11(_ sender: Any) {
+    }
+    @IBAction func dragView12(_ sender: Any) {
+    }
+    @IBAction func dragView13(_ sender: Any) {
+    }
+    @IBAction func dragView14(_ sender: Any) {
+    }
+    @IBAction func dragView15(_ sender: Any) {
+    }
+    let panGesture = UIPanGestureRecognizer(target: self,action: #selector(@objc func handlePan(_:)))
+    dragView.addGestureRecognizer(panGesture)
+    let panGesture = UIPanGestureRecognizer(target: self,action: #selector(@objc func(handlePan(_:)))
+    dragView2.addGestureRecognizer(panGesture)
+    let panGesture = UIPanGestureRecognizer(target: self,action: #selector(@objc func(handlePan(_:)))
+    dragView3.addGestureRecognizer(panGesture)
+    let panGesture = UIPanGestureRecognizer(target: self,action: #selector(@objc func(handlePan(_:)))
+    dragView4.addGestureRecognizer(panGesture)
+    let panGesture = UIPanGestureRecognizer(target: self,action: #selector(@objc func(handlePan(_:)))
+    dragView5.addGestureRecognizer(panGesture)
+    let panGesture = UIPanGestureRecognizer(target: self,action: #selector(@objc func(handlePan(_:)))
+    dragView6.addGestureRecognizer(panGesture)
+    let panGesture = UIPanGestureRecognizer(target: self,action: #selector(@objc func(handlePan(_:)))
+    dragView7.addGestureRecognizer(panGesture)
+    let panGesture = UIPanGestureRecognizer(target: self,action: #selector(@objc func(handlePan(_:)))
+    dragView8.addGestureRecognizer(panGesture)
+    let panGesture = UIPanGestureRecognizer(target: self,action: #selector(@objc func(handlePan(_:)))
+    dragView9.addGestureRecognizer(panGesture)
+    let panGesture = UIPanGestureRecognizer(target: self,action: #selector(@objc func(handlePan(_:)))
+    dragView10.addGestureRecognizer(panGesture)
+    let panGesture = UIPanGestureRecognizer(target: self,action: #selector(@objc func(handlePan(_:)))
+    dragView11.addGestureRecognizer(panGesture)
+    let panGesture = UIPanGestureRecognizer(target: self,action: #selector(@objc func(handlePan(_:)))
+    dragView12.addGestureRecognizer(panGesture)
+    let panGesture = UIPanGestureRecognizer(target: self,action: #selector(@objc func(handlePan(_:)))
+    dragView13.addGestureRecognizer(panGesture)
+    let panGesture = UIPanGestureRecognizer(target: self,action: #selector(@objc func(handlePan(_:)))
+    dragView14.addGestureRecognizer(panGesture)
+    let panGesture = UIPanGestureRecognizer(target: self,action: #selector(@objc func(handlePan(_:)))
+    dragView15.addGestureRecognizer(panGesture)
+    
+    
+    @objc func handlePan(_) gesture: UIPanGestureRecognizer) (-
+    let translation = gesture.translation(in: view)~
+    if let gestureView = gesture.view ( (-))
+    gestureview.center = CGPoint(-)
+    x: gestureView.center.x + translation.x,-
+    y: gestureview.center.y + translation.y-
+    
+    gesture.setTranslation(.zero,in:)
+                           
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+                           
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
