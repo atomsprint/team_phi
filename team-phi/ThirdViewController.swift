e1b66b6e7e889ed0c26db4a3b0c63c5057f8e385
iwamotoousuke <ousuke.i2012@gmail.com>
2026-04-04 06:30:34 +0900

    ThirdViewControllerコード編集

--- team-phi/ThirdViewController.swift
+++ team-phi/ThirdViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ThirdViewController: UIViewController {
     
+    @IBOutlet weak var sofa_view: UIView!
+    @IBOutlet weak var bed_view: UIView!
+    @IBOutlet weak var chair_view: UIView!
+    @IBOutlet weak var table_view: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
-    }
-    
-  }
+        let dragView = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+                dragView.backgroundColor = .systemBlue
+                dragView.isUserInteractionEnabled = true
+                view.addSubview(dragView)
+                
+                let panGesture = UIPanGestureRecognizer(target: self, action: #selector(dragView(_:)))
+                dragView.addGestureRecognizer(panGesture)
+            }
+
+            @objc func dragView(_ sender: UIPanGestureRecognizer) {
+                guard let targetView = sender.view else { return }
+                
+                let translation = sender.translation(in: view)
+                
+                targetView.center = CGPoint(
+                    x: targetView.center.x + translation.x,
+                    y: targetView.center.y + translation.y
+                )
+                
+                sender.setTranslation(.zero, in: view)
+            }
+        }
 
