2934de923161904b493429e20c7c2abe9037a97b
yoshi1029 <yo.it.9201@gmail.com>
2026-04-11 13:39:52 +0900

    グレーの上かどうかの判定

--- team-phi/SecondViewController.swift
+++ team-phi/SecondViewController.swift
@@ -9,31 +9,32 @@ import UIKit
 
 class SecondViewController: UIViewController, UIGestureRecognizerDelegate {
 
-    @IBOutlet weak var image1: UIView!
-    @IBOutlet weak var image2: UIView!
-    @IBOutlet weak var image3: UIView!
-    @IBOutlet weak var image15: UIImageView!
-    @IBOutlet weak var image25: UIImageView!
-    @IBOutlet weak var image35: UIImageView!
+    @IBOutlet var roomview: UIView!
+    @IBOutlet var image1: UIView!
+    @IBOutlet var image2: UIView!
+    @IBOutlet var image3: UIView!
+    @IBOutlet var image15: UIImageView!
+    @IBOutlet var image25: UIImageView!
+    @IBOutlet var image35: UIImageView!
     
 
    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         let panGesture1 = UIPanGestureRecognizer(target: self, action: #selector(handlepan(_:)))
         image1.addGestureRecognizer(panGesture1)
-       
+        
         let panGesture2 = UIPanGestureRecognizer(target: self, action: #selector(handlepan(_:)))
         image2.addGestureRecognizer(panGesture2)
-       
+        
         let panGesture3 = UIPanGestureRecognizer(target: self, action: #selector(handlepan(_:)))
         image3.addGestureRecognizer(panGesture3)
-      
+        
         let panGesture15 = UIPanGestureRecognizer(target: self, action: #selector(handlepan(_:)))
         image15.addGestureRecognizer(panGesture15)
-      
+        
         let panGesture25 = UIPanGestureRecognizer(target: self, action: #selector(handlepan(_:)))
         image25.addGestureRecognizer(panGesture25)
         
@@ -42,7 +43,7 @@ class SecondViewController: UIViewController, UIGestureRecognizerDelegate {
         
         let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(handlePinch(_:)))
         pinchGesture.delegate = self
-    
+        
         // image1用
         let pinch1 = UIPinchGestureRecognizer(target: self, action: #selector(handlePinch(_:)))
         pinch1.delegate = self
@@ -61,12 +62,12 @@ class SecondViewController: UIViewController, UIGestureRecognizerDelegate {
         let pinch15 = UIPinchGestureRecognizer(target: self, action: #selector(handlePinch(_:)))
         pinch15.delegate = self
         image15.addGestureRecognizer(pinch15)
-
+        
         // image25用
         let pinch25 = UIPinchGestureRecognizer(target: self, action: #selector(handlePinch(_:)))
         pinch25.delegate = self
         image25.addGestureRecognizer(pinch25)
-
+        
         // image35用
         let pinch35 = UIPinchGestureRecognizer(target: self, action: #selector(handlePinch(_:)))
         pinch35.delegate = self
@@ -92,6 +93,56 @@ class SecondViewController: UIViewController, UIGestureRecognizerDelegate {
         }
         
         gesture.setTranslation(.zero, in: view)
+        
+        //roomviewに乗っているかの判定
+        guard let movingview  = gesture.view else { return }
+        if gesture.state == .ended {
+                // movingviewたちの中心がroomviewの枠内にあるか判定
+                let centerInTarget = movingview.superview?.convert(movingview.center, to: roomview) ?? .zero
+                
+            if roomview.bounds.contains(centerInTarget) {
+                // --- 【載っていたら】 ---
+                print("設置完了")
+                
+            }else {
+                // --- 【載っていなかったら】 ---
+                    print("元に戻った")
+                
+                //image1を戻す
+            if movingview == image1{
+                image1.frame = CGRect(x: 16, y: 306, width: 156, height: 95)
+                view.addSubview(image1)
+            }
+                //image2を戻す
+            if movingview == image2{
+                image2.frame = CGRect(x: 212, y: 306, width: 156, height: 95)
+                view.addSubview(image2)
+            }
+                //image3を戻す
+            if movingview == image3{
+                image3.frame = CGRect(x: 21, y: 409, width: 156, height: 95)
+                view.addSubview(image3)
+            }
+                //image15を戻す
+            if movingview == image15{
+                image15.frame = CGRect(x: 30.5, y: 509.5, width: 95, height: 156)
+                view.addSubview(image15)
+            }
+                //image25を戻す
+            if movingview == image25{
+                image25.frame = CGRect(x: 153.5, y: 509.5, width: 95, height: 156)
+                view.addSubview(image25)
+            }
+                //image35を戻す
+            if movingview == image35{
+                image35.frame = CGRect(x: 278.5, y: 509.5, width: 95, height: 156)
+                view.addSubview(image35)
+            }
+                
+                
+                }
+            }
+        
     }
     @objc func handlePinch(_ gesture: UIPinchGestureRecognizer) {
         if let gestureView = gesture.view {
