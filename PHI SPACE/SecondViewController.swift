26aa2bf899e5103c638127150bbf1b16bdccbe0b
taka-hilo <2310dinosaur@gmail.com>
2026-05-26 17:28:18 +0900

    すいません遷移出来てないです

--- PHI SPACE/SecondViewController.swift
+++ PHI SPACE/SecondViewController.swift
@@ -33,4 +33,40 @@ class SecondViewController: UIViewController {
         // 四角形をビューに追加
         view.addSubview(shape)
     }
+    
+    @IBAction func showAlertButtonTapped(_ sender: UIButton) {
+        // 1. アラートの器（タイトルとメッセージ）を作る
+        let alert = UIAlertController(
+            title: "確認",
+            message: "このサイズでいいですか",
+            preferredStyle: .alert
+        )
+        
+        //2. OKボタンの処理
+        let okAction = UIAlertAction(title: "はい", style: .default) { _ in
+            // 1. ストーリーボードから次の画面を取得（Storyboard IDを設定しておく必要があります）
+            if let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "shapeViewcontroller") {
+                
+                // 2-A. 下から上にポップアップ表示したい場合（Modal）
+                // self.present(nextVC, animated: true, completion: nil)
+                
+                // 2-B. 横からスライドして進みたい場合（NavigationControllerを使っている場合）
+                 self.navigationController?.pushViewController(nextVC, animated: true)
+            }
+        }
+        
+        // キャンセルボタンの処理
+        let cancelAction = UIAlertAction(title: "キャンセル", style: .cancel) { _ in
+            // キャンセルされた時に背景色を白にする
+            self.shape.backgroundColor = .white
+        }
+        
+        // 4. アラートにボタンを追加する
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        
+        // 5. 画面にアラートを表示する
+        present(alert, animated: true, completion: nil)
+    }
 }
+
